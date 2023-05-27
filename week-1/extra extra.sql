1086679b645e632f7207dcfa6fd514aa20cc83cb
saima-khan1 <saimakhan.6979@gmail.com>
2023-05-27 14:14:51 +0200

    extra queries added

---
+++ week-1/extra extra.sql
@@ -0,0 +1,21 @@
+-- 1. What are the names of the countries that has more than 10 cities, and a total 
+-- population of the cities listed (not the country population) of more than 50 million?
+select country.name
+from country
+inner join city on country.code = city.countrycode
+group by country.name
+having count(city.name) > 10 and sum(city.population) > 50000000
+order by sum(city.population) asc;
+
+-- 2. List the cities from those countries, where the city population is > 5M
+select city.name, city.population
+from city
+where countrycode in (
+  select country.code
+  from country
+  inner join city on country.code = city.countrycode
+  group by country.code
+  having count(city.name) > 10 and sum(city.population) > 50000000
+)
+and city.population > 5000000
+order by population asc;
