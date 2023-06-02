644e505ea491acc53435de5bd8f45ab45ddb519f
saima-khan1 <saimakhan.6979@gmail.com>
2023-06-02 11:20:21 +0200

    updates on part-1
    made changes to use connection.prepare and connection.execute

--- week-2/part-1.js
+++ week-2/part-1.js
@@ -1,36 +1,27 @@
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';
 import prompt from 'prompt';
 import fs from 'fs';
 
-var connection = mysql.createConnection({
+const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root1',
   password: 'Hamzaan06',
 });
-var preparedStatements = '';
 
 // Connect to the database
-connection.connect((error) => {
-  if (error) {
-    console.error('Error connecting to the database:', error);
-    return;
-  }
-
+try {
   console.log('Connected to the database.');
   // Use the new_world database
-  connection.query('USE new_world', (err) => {
-    if (err) {
-      console.error('Error using the new_world database:', err);
-      return;
-    }
-  });
+  await connection.query('USE new_world');
 
   // Start the prompt
   prompt.start();
 
   // Call the function to display options
   displayOptions();
-});
+} catch (error) {
+  console.error('Error connecting to the database:', error);
+}
 
 // Function to display the menu options
 function displayOptions() {
@@ -87,17 +78,14 @@ const query1 =
 const query2 =
   'select DISTINCT Language, region from country c join countrylanguage cl on c.code=cl.CountryCode where region = ?';
 const query3 =
-  'select count(ct.name) as cities ,cl.Language from countrylanguage cl join city ct using(countrycode) where Language= ? ';
+  'SELECT COUNT(ct.name) AS cities, cl.Language FROM countrylanguage cl JOIN city ct USING (countrycode) WHERE Language = ? GROUP BY cl.Language';
 const query4 =
   'select c.Continent , count(DISTINCT cl.Language) as numberoflanguage from country c join countrylanguage cl on c.code= cl.CountryCode GROUP BY c.Continent';
 const query5 = `select c2.Name as CountryName, cl2.Language from country c1 join country c2 ON c1.Continent = c2.Continent join countrylanguage cl1 on c1.Code = cl1.CountryCode join countrylanguage cl2 on c2.Code = cl2.CountryCode where c1.Name = ? and c2.Name != ? and cl1.Language = cl2.Language`;
 
 // Concatenate the prepared statements to the string variable
-preparedStatements += query1 + '\n';
-preparedStatements += query2 + '\n';
-preparedStatements += query3 + '\n';
-preparedStatements += query4 + '\n';
-preparedStatements += query5 + '\n';
+const preparedStatements =
+  query1 + '\n' + query2 + '\n' + query3 + '\n' + query4 + '\n' + query5;
 
 // Write the prepared statements to a file
 fs.writeFile('prepared_statements.txt', preparedStatements, (error) => {
@@ -112,7 +100,7 @@ fs.writeFile('prepared_statements.txt', preparedStatements, (error) => {
 // Prompt for country name and get capital
 function promptForCountryCapital() {
   console.log('Enter the name of the country:');
-  prompt.get(['countryName'], (error, result) => {
+  prompt.get(['countryName'], async (error, result) => {
     if (error) {
       console.error('Error while getting user input:', error);
       return;
@@ -120,121 +108,21 @@ function promptForCountryCapital() {
 
     const countryName = result.countryName;
 
-    // Prepare the statement query
-    const query = query1;
-    const values = [countryName];
+    try {
+      // Prepare the statement query
+      const statement = await connection.prepare(query1);
 
-    // Execute the prepared statement
-    connection.query(query, values, (error, results) => {
-      if (error) {
-        console.error('Error executing the query:', error);
-        return;
-      }
+      // Execute the prepared statement
+      const [rows] = await statement.execute([countryName]);
 
-      if (results.length > 0) {
-        const capital = results[0].Name;
+      if (rows.length > 0) {
+        const capital = rows[0].Name;
         console.log(`The capital of ${countryName} is ${capital}.`);
       } else {
         console.log(`No results found for ${countryName}.`);
       }
-
-      // Call the function to display options again
-      displayOptions();
-    });
-  });
-}
-
-// Prompt for region name and list languages spoken
-function promptForRegionLanguages() {
-  console.log('Enter the name of the region:');
-  prompt.get(['regionName'], (error, result) => {
-    if (error) {
-      console.error('Error while getting user input:', error);
-      return;
-    }
-
-    const regionName = result.regionName;
-
-    // Prepare the statement query
-    const query = query2;
-    const values = [regionName];
-
-    // Execute the prepared statement
-    connection.query(query, values, (error, results) => {
-      if (error) {
-        console.error('Error executing the query:', error);
-        return;
-      }
-
-      if (results.length > 0) {
-        const languages = results.map((obj) => obj.Language);
-        console.log(`Languages spoken in ${regionName}: ${languages}`);
-      } else {
-        console.log(`No results found for ${regionName}.`);
-      }
-
-      // Call the function to display options again
-      displayOptions();
-    });
-  });
-}
-
-// Prompt for language name and get the number of cities
-function promptForLanguageCities() {
-  console.log('Enter the name of the language:');
-  prompt.get(['languageName'], (error, result) => {
-    if (error) {
-      console.error('Error while getting user input:', error);
-      return;
-    }
-
-    const languageName = result.languageName;
-
-    // Prepare the statement query
-    const query = query3;
-    const values = [languageName];
-
-    // Execute the prepared statement
-    connection.query(query, values, (error, results) => {
-      if (error) {
-        console.error('Error executing the query:', error);
-        return;
-      }
-
-      if (results.length > 0) {
-        const cityCount = results[0].cities;
-        console.log(
-          `Number of cities where ${languageName} is spoken: ${cityCount}`
-        );
-      } else {
-        console.log(`No results found for ${languageName}.`);
-      }
-
-      // Call the function to display options again
-      displayOptions();
-    });
-  });
-}
-
-// List continents with the number of languages spoken
-function listContinentsWithLanguages() {
-  // Prepare the statement query
-  const query = query4;
-
-  // Execute the prepared statement
-  connection.query(query, (error, results) => {
-    if (error) {
+    } catch (error) {
       console.error('Error executing the query:', error);
-      return;
-    }
-
-    if (results.length > 0) {
-      console.log('Continents with the number of languages spoken:');
-      results.forEach((row) =>
-        console.log(`${row.Continent}: ${row.numberoflanguage}`)
-      );
-    } else {
-      console.log('No results found.');
     }
 
     // Call the function to display options again
@@ -242,9 +130,102 @@ function listContinentsWithLanguages() {
   });
 }
 
-function listCountriesWithSameLanguageAndContinent() {
+// Prompt for region name and list languages spoken
+function promptForRegionLanguages() {
+  console.log('Enter the name of the region:');
+  prompt.get(['regionName'], async (error, result) => {
+    if (error) {
+      console.error('Error while getting user input:', error);
+      return;
+    }
+
+    const regionName = result.regionName;
+
+    try {
+      // Prepare the statement query
+      const statement = await connection.prepare(query2);
+
+      // Execute the prepared statement
+      const [rows] = await statement.execute([regionName]);
+
+      if (rows.length > 0) {
+        const languages = rows.map((obj) => obj.Language);
+        console.log(`Languages spoken in ${regionName}: ${languages}`);
+      } else {
+        console.log(`No results found for ${regionName}.`);
+      }
+    } catch (error) {
+      console.error('Error executing the query:', error);
+    }
+
+    // Call the function to display options again
+    displayOptions();
+  });
+}
+
+// Prompt for language name and get the number of cities
+function promptForLanguageCities() {
+  console.log('Enter the name of the language:');
+  prompt.get(['languageName'], async (error, result) => {
+    if (error) {
+      console.error('Error while getting user input:', error);
+      return;
+    }
+
+    const languageName = result.languageName;
+
+    try {
+      // Prepare the statement query
+      const statement = await connection.prepare(query3);
+
+      // Execute the prepared statement
+      const [rows] = await statement.execute([languageName]);
+
+      if (rows.length > 0) {
+        const cityCount = rows[0].cities;
+        console.log(
+          `Number of cities where ${languageName} is spoken: ${cityCount}`
+        );
+      } else {
+        console.log(`No results found for ${languageName}.`);
+      }
+    } catch (error) {
+      console.error('Error executing the query:', error);
+    }
+
+    // Call the function to display options again
+    displayOptions();
+  });
+}
+
+// List continents with the number of languages spoken
+async function listContinentsWithLanguages() {
+  try {
+    // Prepare the statement query
+    const statement = await connection.prepare(query4);
+
+    // Execute the prepared statement
+    const [rows] = await statement.execute();
+
+    if (rows.length > 0) {
+      console.log('Continents with the number of languages spoken:');
+      rows.forEach((row) =>
+        console.log(`${row.Continent}: ${row.numberoflanguage}`)
+      );
+    } else {
+      console.log('No results found.');
+    }
+  } catch (error) {
+    console.error('Error executing the query:', error);
+  }
+
+  // Call the function to display options again
+  displayOptions();
+}
+
+async function listCountriesWithSameLanguageAndContinent() {
   console.log('Enter the name of the country:');
-  prompt.get(['countryName'], (error, result) => {
+  prompt.get(['countryName'], async (error, result) => {
     if (error) {
       console.error('Error while getting user input:', error);
       return;
@@ -252,30 +233,28 @@ function listCountriesWithSameLanguageAndContinent() {
 
     const countryName = result.countryName;
 
-    // Prepare the statement query
-    const query = query5;
-    const values = [countryName, countryName];
+    try {
+      // Prepare the statement query
+      const statement = await connection.prepare(query5);
 
-    // Execute the prepared statement
-    connection.query(query, values, (error, results) => {
-      if (error) {
-        console.error('Error executing the query:', error);
-        return;
-      }
+      // Execute the prepared statement
+      const [rows] = await statement.execute([countryName, countryName]);
 
-      if (results.length > 0) {
+      if (rows.length > 0) {
         console.log(
           `Countries with the same official language as ${countryName} in the same continent:`
         );
-        results.forEach((row) =>
+        rows.forEach((row) =>
           console.log(`${row.CountryName}: ${row.Language}`)
         );
       } else {
         console.log(`No results found for ${countryName}.`);
       }
+    } catch (error) {
+      console.error('Error executing the query:', error);
+    }
 
-      // Call the function to display options again
-      displayOptions();
-    });
+    // Call the function to display options again
+    displayOptions();
   });
 }
